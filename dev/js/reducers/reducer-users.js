e12b66fdec1a655484a765a4f6c8f1b2ff58c68e
ahale212 <dalefarm@hotmail.co.uk>
2017-11-12 17:14:33 +0000

    working app

---
+++ dev/js/reducers/reducer-users.js
@@ -0,0 +1,21 @@
+export default function () {
+    return [
+        {
+            id: 1,
+            first: "Foo",
+            last: "Bar",
+            age: 24,
+            desc: "Foo is a React developer",
+            thumbnail: "..."
+        },
+        {
+            id: 2,
+            first: "Joe",
+            last: "Blogs",
+            age: 23,
+            desc: "Joe is not a React developer",
+            thumbnail: "..."
+        },
+
+    ]
+}
